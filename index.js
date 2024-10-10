b36096d684111bc0bf501ac70aa7ea51c85af1f0
Kittipong Prasompong <kittipongprasompong@gmail.com>
2024-10-10 08:22:02 +0000

    implement api products

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Request-With, Content-Type, Accept, Authorization")
-  next();
+  next()
 })
 
 const products=[
